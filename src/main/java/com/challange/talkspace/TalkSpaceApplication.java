41821e9d25c06a4c4cbc5b58c4da1925084aed38
Mykola Skrypka <mykolaskrypka@gmail.com>
2023-07-12 00:01:49 +0300

    first commit

---
+++ src/main/java/com/challange/talkspace/TalkSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.challange.talkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkSpaceApplication.class, args);
+    }
+
+}
